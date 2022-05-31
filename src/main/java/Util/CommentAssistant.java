cc2f2c5f2239b9c8d6ef04c2ed8a638ab4e553f7
rmorerahul888 <rahul.more@trionxt.com>
2022-05-31 15:25:41 +0530

    resolve issue multiple duplicate comments

--- src/main/java/Util/CommentAssistant.java
+++ src/main/java/Util/CommentAssistant.java
@@ -1,9 +1,11 @@
 package Util;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Field;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -27,33 +29,50 @@ public class CommentAssistant {
         this.logger = logger;
     }
 
-    public String addSystemData(String query) {
-        JSONObject jsonObject = new JSONObject();
-        try {
-            Field changeMap = jsonObject.getClass().getDeclaredField("map");
-            changeMap.setAccessible(true);
-            changeMap.set(jsonObject, new LinkedHashMap<>());
-            changeMap.setAccessible(false);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
+	public String addSystemData(String query) {
+		StackTraceElement stackTraceElement=getStackTraceElement();
+		JSONObject jsonObject = new JSONObject();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
 
-        }
-        setVariables();
+		// Gets the current time
+		LocalDateTime now = LocalDateTime.now();
+		Class<?> c = null;
+		try {
+			c = Class.forName(stackTraceElement.getClassName());
+			iP = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+//		try {
+//			Field changeMap = jsonObject.getClass().getDeclaredField("map");
+//			changeMap.setAccessible(true);
+//			changeMap.set(jsonObject, new LinkedHashMap<>());
+//			changeMap.setAccessible(false);
+//		} catch (IllegalAccessException | NoSuchFieldException e) {
+//
+//		}
+		// setVariables();
+		
+		// Put in the order that they are in the JSON object
+		jsonObject.put("IP_ADDRESS", iP);
+		jsonObject.put("METHODNAME",stackTraceElement.getMethodName());
+		jsonObject.put("CLASSNAME", stackTraceElement.getClassName());
+		jsonObject.put("CURRENT_TIME", dtf.format(now));
+		jsonObject.put("PACKAGENAME", c != null ? c.getPackage().getName() : "");
 
-        // Put in the order that they are in the JSON object
-        jsonObject.put("IP_ADDRESS", iP);
-        jsonObject.put("METHODNAME", methodName);
-        jsonObject.put("CLASSNAME", className);
-        jsonObject.put("CURRENT_TIME", time);
-        jsonObject.put("PACKAGENAME", packageName);
+	//	System.out.println(jsonObject);
+		if(!query.contains(" /*" + jsonObject + "*/")){
+				finalString = query.concat(" /*" + jsonObject + "*/");
+		}
+		System.out.println(finalString);
 
-        System.out.println(jsonObject);
-
-        finalString = query.concat(" /*" + jsonObject + "*/");
-
-        System.out.println(finalString);
-
-        return finalString;
-        }
+		return finalString;
+	}
 
 
     private void setVariables() {
@@ -101,7 +120,7 @@ public class CommentAssistant {
         time = dtf.format(now);
     }
 
-    private StackTraceElement getStackTraceElement() {
+    public static StackTraceElement getStackTraceElement() {
 
         Class <?> c;
         Thread thread = Thread.currentThread();
@@ -111,12 +130,12 @@ public class CommentAssistant {
             for (StackTraceElement element : stacktrace) {
                 c = Class.forName(element.getClassName());
                 String packagename = c.getPackage().getName();
-                if (!packagename.equals("Util") && !packagename.equals("NamedJDBC") && !packagename.equals("java.lang")) {
+                if (!packagename.equals("Util")&& !packagename.equals("org.springframework.jdbc.core.namedparam") && !packagename.equals("NamedJDBC") && !packagename.equals("java.lang")) {
                     return element;
                 }
             }
         } catch (ClassNotFoundException e) {
-            logger.log(Level.WARNING, e.getMessage());
+          //  logger.log(Level.WARNING, e.getMessage());
         }
 
         return null;
